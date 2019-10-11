0cead9c7acb4d61fcd75ed0314030534b6f514a2
johinsdev <johinsdev@gmail.com>
2019-10-11 18:15:35 -0500

    add travis

---
+++ index.test.js
@@ -0,0 +1,39 @@
+/*
+ * index.test.js
+ * Copyright (C) 2019 johan.villamilchiper.com <johan.villamilchiper.com@Johans-iMac.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+(function(){
+  'use strict';
+  
+  const expect = require('chai').expect
+
+  const starwars = require('./index.js')
+
+  describe('starwars-names', function () {
+    describe('all', function() { 
+      it('should be an array of strings', function () {
+        expect(starwars.all).to.satisfy(isArrayOfStrings)
+
+        function isArrayOfStrings(array) {
+          return array.every(function (item) {
+            return typeof item === 'string'
+          }) 
+        }
+      })
+
+      it('sould contain `Luke Skywalwer`', function () {
+        expect(starwars.all).to.include('Luke Skywalker')
+      })
+    })
+
+    describe('random', function () {
+      it('should return a random item from the starwars all.', function () {
+        const randomItem = starwars.random()
+
+        expect(starwars.all).to.include(randomItem)
+      }) 
+    })
+  })
+})();
